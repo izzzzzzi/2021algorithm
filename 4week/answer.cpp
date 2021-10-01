6c34544cdbbfe9aea9dfa40a5a29c3de34c2a947
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2021-10-01 15:25:43 +0900

    Update answer.cpp

--- 4week/answer.cpp
+++ 4week/answer.cpp
@@ -1,16 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Solution{
-public:
-	void pushZerosToEnd(int arr[], int n) {
-	    int count = 0,i;
-	    for(i=0;i<n;i++)
-	    {
-	        if(arr[i]>0)
-	          arr[count++] = arr[i];
-      }
-	     while(count<n)
-	           arr[count++] = 0;
-	}
-};
+
+void moveZerosToEnd(int arr[], int n)
+{
+	int count = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] != 0)
+			swap(arr[count++], arr[i]);
+}
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+}
+
+int main()
+{
+	int arr[] = {6, 0, 8, 2, 3, 0, 4, 0, 1};
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	moveZerosToEnd(arr, n);
+	printArray(arr, n);
+
+	return 0;
+}
