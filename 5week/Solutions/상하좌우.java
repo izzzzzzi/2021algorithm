a303686e11489ffcac92578759b541c727209d96
izzzzzzi <78688607+izzzzzzi@users.noreply.github.com>
2021-10-07 19:22:54 +0900

    Update 상하좌우.java

--- 5week/Solutions/상하좌우.java
+++ 5week/Solutions/상하좌우.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Main { 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in); 
-        
+        // 1. 입력 받기
         int n = sc.nextInt(); sc.nextLine(); 
         String[] plans = sc.nextLine().split(" "); 
         int x = 1, y = 1; 
         
+        // 2. LRUD 이동방향 설정
         int[] dx = {0, 0, -1, 1}; 
         int[] dy = {-1, 1, 0, 0}; 
         char[] moveTypes = {'L', 'R', 'U', 'D'}; 
